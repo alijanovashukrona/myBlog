b77228efcbbc89aa247bf5fe66d8616248a1b696
alijanovashukrona <146185050+alijanovashukrona@users.noreply.github.com>
2024-04-12 17:45:02 +0500

    delete function

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+
 
 # Create your views here.
 
@@ -22,4 +24,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
